601338a1b48da7b661c259e40b4a22cc5d0f6309
DavHau <hsngrmpf+github@gmail.com>
2022-02-24 17:45:24 +0700

    support different passwords for datasets

--- automount.sh
+++ automount.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Enter your encrypted datasets here
-enc_datasets="pool11/enc rpool/enc"
 provider_host="10.99.99.2"
+enc_datasets=$(zfs get -r -H -o name,value keyformat | grep passphrase | awk '{print $1}' | xargs)
 
 set -e
 for ds in $enc_datasets; do
--- provide_password.sh
+++ provide_password.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 provider_host="10.99.99.2"
+enc_datasets=$(zfs get -r -H -o name,value keyformat | grep passphrase | awk '{print $1}' | xargs)
 
-read -s passwd
-ssh root@$provider_host "echo $passwd > /run/zfs_passwd"
+for ds in $enc_datasets; do
+  location="/run/zfs_passwords/$ds"
+  echo "please enter password for $ds:"
+  read -s passwd
+  ssh root@$provider_host mkdir -p $(dirname $location)
+  ssh root@$provider_host "echo $passwd > $location"
+done
