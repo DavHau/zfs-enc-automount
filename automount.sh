f3f4a667588534e40fc7faf280f3053197ed5035
DavHau <hsngrmpf+github@gmail.com>
2022-02-24 17:38:07 +0700

    support different password for each dataset

--- automount.sh
+++ automount.sh
@@ -5,8 +5,9 @@ enc_datasets="pool11/enc rpool/enc"
 provider_host="10.99.99.2"
 
 set -e
-passwd=$(ssh root@$provider_host cat /run/zfs_passwd)
 for ds in $enc_datasets; do
+  location="/run/zfs_passwords/$ds"
+  passwd=$(ssh root@$provider_host cat $location)
   echo $passwd | zfs load-key $ds && echo "key loaded successfully"
 done
 zfs mount -a && echo "all datasets mounted successfully"
